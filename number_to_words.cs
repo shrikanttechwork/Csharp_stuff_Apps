bfc291f17f1fd6030c5d7be9f4ed4ed27d5a7db3
shrikanttechwork <shrimahajan0@gmail.com>
2018-02-20 23:59:18 +0530

    First Commit - Added number to word c# script

---
+++ number_to_words.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_stuff_Apps
+{
+    class Number_to_words
+    {
+        public static void Main()
+        {
+            string input;
+            int number;
+            bool isValid;
+            bool isUK = false;
+            Console.WriteLine("\nEnter '0' to quit the program at any time\n");
+            while (true)
+            {
+                Console.Write("\nUse UK numbering y/n : ");
+                input = Console.ReadLine();
+                if (!(input.ToLower() == "y" || input.ToLower() == "n"))
+                    Console.WriteLine("\n  Must be 'y' or 'n', please try again\n");
+                else
+                {
+                    if (input.ToLower() == "y") isUK = true;
+                    Console.WriteLine("\n");
+                    break;
+                }
+            }
+            do
+            {
+                Console.Write("Enter integer : ");
+                input = Console.ReadLine();
+                isValid = int.TryParse(input, out number);
+                if (!isValid)
+                    Console.WriteLine("\n  Not an integer, please try again\n");
+                else
+                    Console.WriteLine("\n  {0}\n", NumberToText(number, isUK));
+            }
+            while (!(isValid && number == 0));
+            Console.WriteLine("\nProgram ended");
+        }
+        public static string NumberToText(int number, bool isUK)
+        {
+            if (number == 0) return "Zero";
+            string and = isUK ? "and " : ""; // deals with UK or US numbering
+            if (number == -2147483648) return "Minus Two Billion One Hundred " + and +
+            "Forty Seven Million Four Hundred " + and + "Eighty Three Thousand " +
+            "Six Hundred " + and + "Forty Eight";
+            int[] num = new int[4];
+            int first = 0;
+            int u, h, t;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            if (number < 0)
+            {
+                sb.Append("Minus ");
+                number = -number;
+            }
+            string[] words0 = { "", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine " };
+            string[] words1 = { "Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen " };
+            string[] words2 = { "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety " };
+            string[] words3 = { "Thousand ", "Million ", "Billion " };
+            num[0] = number % 1000;           // units
+            num[1] = number / 1000;
+            num[2] = number / 1000000;
+            num[1] = num[1] - 1000 * num[2];  // thousands
+            num[3] = number / 1000000000;     // billions
+            num[2] = num[2] - 1000 * num[3];  // millions
+            for (int i = 3; i > 0; i--)
+            {
+                if (num[i] != 0)
+                {
+                    first = i;
+                    break;
+                }
+            }
+            for (int i = first; i >= 0; i--)
+            {
+                if (num[i] == 0) continue;
+                u = num[i] % 10;              // ones
+                t = num[i] / 10;
+                h = num[i] / 100;             // hundreds
+                t = t - 10 * h;               // tens
+                if (h > 0) sb.Append(words0[h] + "Hundred ");
+                if (u > 0 || t > 0)
+                {
+                    if (h > 0 || i < first) sb.Append(and);
+                    if (t == 0)
+                        sb.Append(words0[u]);
+                    else if (t == 1)
+                        sb.Append(words1[u]);
+                    else
+                        sb.Append(words2[t - 2] + words0[u]);
+                }
+                if (i != 0) sb.Append(words3[i - 1]);
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
